bc28422a28f561a109137680dadf11b5ea8f8a4b
Jack Lingle <captainjack0818@gmail.com>
2024-01-31 19:45:48 -0500

    add post route to users

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../data/models/user');
 
 const router = express.Router();
@@ -14,7 +15,25 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
+  const {
+    username, password, organizations, firstName, lastName,
+  } = req.body;
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
 
+    const user = new User({
+      username,
+      passwordHash,
+      organizations,
+      firstName,
+      lastName,
+      tickets: [],
+    });
+    const savedUser = await user.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
