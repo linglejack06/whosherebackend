20e6821cd282e87dc8d5959f4ad0cd595b137e0c
Jack Lingle <captainjack0818@gmail.com>
2024-02-11 11:02:16 -0500

    implement basic authentication in websockets

--- websockets/websocket.js
+++ websockets/websocket.js
@@ -1,27 +1,40 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const ws = require('ws');
 const uniqid = require('uniqid');
+const jwt = require('jsonwebtoken');
 
 const wsServer = new ws.Server({ noServer: true });
 const clients = new Map();
 
-const acceptMessage = (socket, msg) => {
-  const messageJSON = JSON.parse(msg);
-  const metadata = clients.get(socket);
-  messageJSON.sender = metadata.id;
-  const outbound = JSON.stringify(messageJSON);
-  [...clients.keys()].forEach((client) => {
-    client.send(outbound);
-  });
+const authenticate = async (token) => {
+  const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
+  return decodedToken != null;
 };
-wsServer.on('connection', (socket) => {
+const acceptMessage = (id, msg) => {
+  const messageJSON = JSON.parse(msg);
+  const metadata = clients.get(id);
+  if (msg.type === 'auth') {
+    metadata.authenticated = authenticate(msg.token);
+    return;
+  }
+
+  // only allow messages if authenticated
+  if (metadata.authenticated) {
+    messageJSON.sender = metadata.id;
+    const outbound = JSON.stringify(messageJSON);
+    [...clients.keys()].forEach((client) => {
+      client.send(outbound);
+    });
+  }
+};
+
+wsServer.on('connection', (socket, request) => {
   const id = uniqid();
-  const metadata = { id };
+  const metadata = { id, userId: request.decodedToken.id };
   clients.set(socket, metadata);
-  console.log('client connected');
 
   socket.on('message', (msg) => {
-    acceptMessage(socket, msg);
+    acceptMessage(id, msg);
   });
 
   socket.on('close', () => {
